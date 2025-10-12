6526c23ff5606bf2bd077c09866657d74e1e6027
CappucinaAssasina <diamondvillager69@gmail.com>
2025-10-12 20:38:42 +0530

    Python 1

---
+++ 1.py
@@ -0,0 +1,8 @@
+Dollar = "💵"
+Yen = "💴"
+Euro = "💶"
+Pound = "💷"
+print(f"This is a Pound {Pound}")
+print(f"This is a Dollar {Dollar}")
+print(f"This is a Yen {Yen}")
+print(f"This is a Euro {Euro}")
