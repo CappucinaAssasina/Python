3503bb817252d91599052a3aa29dd44648fcc2e7
CappucinaAssasina <diamondvillager69@gmail.com>
2025-11-01 21:30:32 +0530

    []

--- 7.py
+++ 7.py
@@ -1,6 +1,36 @@
-money = ["💵","💶","💷","💴"]
+Dollar = {
+    "Country" : "USA",
+    "Symbol" : "$",
+    "Visual" : "💵"
+}
+Yen = {
+    "Country" : "Japan",
+    "Symbol" : "¥",
+    "Visual" : "💴"
+}
+Euro = {
+    "Country" : "France",
+    "Symbol" : "€",
+    "Visual" : "💶"
+}
+Pound = {
+    "Country" : "UK",
+    "Symbol" : "£",
+    "Visual" : "💷"
+}
+Rupee = {
+    "Symbol" : "₹"
+}
 
-print("Dollar: ",money[0])
-print("Euro: ",money[1])
-print("Pound: ",money[2])
-print("Yen: ",money[3])
+money = [Dollar,Yen,Euro,Pound,Rupee]
+
+print(money[0])
+print(money[1])
+print(money[2])
+print(money[3])
+
+Rupee["Country"] = "India"
+print(money[4]["Country"])
+
+del Rupee["Country"]
+print(money[4])
